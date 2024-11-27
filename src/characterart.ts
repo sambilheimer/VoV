78a1cab1d3881b44d4d394dd20947f87790f1082
Sam <sambilheimer@gmail.com>
2024-11-26 23:33:17 -0500

    .gitignore fix

--- src/characterart.ts
+++
@@ -1,9 +0,0 @@
-// Step 7: Randomized character art
-//  let previousImageNumber = null;
-//  let randomImageNumber;
-//  do {
-//      randomImageNumber = Math.floor(Math.random() * selectedAncestry.imageCount) + 1;
-//  } while (randomImageNumber === previousImageNumber);
-// Store the current image number for the next generation
-//  previousImageNumber = randomImageNumber;
-// document.querySelector('.ancestryImg').style.backgroundImage = `url('assets/${selectedAncestryKey}-${randomImageNumber}.jpg')`;
--- src/utils.ts
+++
@@ -1,3 +0,0 @@
-export function getRandom(array: string[]) {
-  return array[Math.floor(Math.random() * array.length)];
-}
